f1968b3e982a1f9bb2570ffa663bd28891b92741
Rishi Raj Jain <rishi18304@iiitd.ac.in>
2023-12-02 21:58:47 +0530

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import vue from '@astrojs/vue'
 import UnoCSS from '@unocss/astro'
 import config from './configs.json'
+import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
   compressHTML: true,
   site: `https://${config.base_domain.prod}`,
-  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
-  integrations: [vue(), UnoCSS({ injectReset: true })],
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp',
+    },
+  },
+  integrations: [
+    vue(),
+    UnoCSS({
+      injectReset: true,
+    }),
+    sitemap(),
+  ],
 })
